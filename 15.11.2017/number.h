bcb8959eae4442ba8710555f98386bd6793e4f9e
DanikSchastny <32484672+DanikSchastny@users.noreply.github.com>
2017-11-28 20:13:34 +0300

    Update number.h

--- 15.11.2017/number.h
+++ 15.11.2017/number.h
@@ -3,6 +3,7 @@ using namespace std;
 
 int addingInt();
 void swap(int *a, int *b);
+void equalResult(int a);
 
 void swap(int *a, int *b)
 {
@@ -21,3 +22,10 @@ int addingInt()
 		if (n > 0) return n;
 	}
 }
+void equalResult(int a)
+{
+	if (a) cout << "They not are equal";
+
+	else cout << "They are equal";
+}
+
